dc291845bf827014cf0a62136b55791cac8b4049
Tanel Pauls <Tanel.Pauls@voco.ee>
2025-04-04 10:45:58 +0300

    3 - andmete pärimine Meals.js komponendis

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,11 +1,27 @@
-const Meals = () => {
-    return (
-        <ul id="meals">
-            { 
-                // list of meals
-            }
-        </ul>
-    )
-}
+import React, { useEffect, useState } from "react";
 
-export default Meals
+const Meals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/meals");
+        if (!response.ok) {
+          throw new Error("Failed to fetch meals");
+        }
+        const data = await response.json();
+        console.log(data);
+        setMeals(data);
+      } catch (err) {
+        console.error("Error fetching meals:", err);
+      }
+    };
+
+    fetchMeals();
+  }, []);
+
+  return <ul></ul>;
+};
+
+export default Meals;
