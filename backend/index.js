bae6b42ba16c117e680afa833c52628e29e70856
Tanel Pauls <Tanel.Pauls@voco.ee>
2025-04-04 09:42:07 +0300

    1 - Header and Meals

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs/promises");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const path = require("path");
 const express = require("express");
 
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,9 +16,23 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/meals", async (req, res) => {
-  const meals = "[]" // data should be read from file
-  res.json(JSON.parse(meals));
+app.get("/meals", (req, res) => {
+  const filePath = path.join(__dirname, "./data/", "meals.json");
+
+  fs.readFile(filePath, "utf8", (err, jsonData) => {
+    if (err) {
+      console.error("Error reading JSON file:", err);
+      return res.status(500).json({ error: "Failed to read data" });
+    }
+    try {
+      const data = JSON.parse(jsonData);
+      console.log(data);
+      res.json(data);
+    } catch (parseError) {
+      console.error("Error parsing JSON:", parseError);
+      res.status(500).json({ error: "Invalid JSON format" });
+    }
+  });
 });
 
 app.use((req, res) => {
@@ -29,4 +43,13 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.listen(3001);
+const startServer = async () => {
+  try {
+    app.listen(3001);
+    console.log(`Server is running on port 3001.`);
+  } catch (error) {
+    console.error("Failed to start the server:", error.message);
+  }
+};
+
+startServer();
--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Header from './components/Header.js'
+import Meals from './components/Meals.js'
+
 const App = () => {
  return (
     <>
+    <Header />
+    <Meals />
      <h1>Food Order App</h1>
     </>
   );
